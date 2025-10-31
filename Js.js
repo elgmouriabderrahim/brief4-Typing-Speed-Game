7f723bf1bf8a8b26a040ec29c625741e949c0225
elgmouri abderrahim <ea.officialbox@gmail.com>
2025-10-31 11:55:07 +0000

    l'ajout de js fichier de faire la selection du contonue

---
+++ Js.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector(".button");
+btn.addEventListener("click", () => {
+    const choice = document.querySelector("input:checked");
+    const container = document.querySelector(".container");
+    const content = document.querySelector(".content");
+    
+});
